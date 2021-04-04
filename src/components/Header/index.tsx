d4ec6f32d60f009852ee8f582a5567bc8895a86e
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-04-04 15:06:18 -0300

    Page Posts

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -10,7 +10,7 @@ export function Header() {
           <a href="/" className={styles.active}>
             Home
           </a>
-          <a href="/">Posts</a>
+          <a href="/posts">Posts</a>
         </nav>
 
         <SignInButton />
