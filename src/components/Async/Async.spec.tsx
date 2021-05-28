cbae48d1734b2c87e912395fa5860e3e7413ef01
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2021-05-27 23:41:03 -0300

    Async testing

---
+++ src/components/Async/Async.spec.tsx
@@ -0,0 +1,22 @@
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
+import { Async } from '.';
+
+test('it renders correctly', async () => {
+  render(<Async />);
+
+  expect(screen.getByText('Hello world')).toBeInTheDocument();
+  // expect(await screen.findByText('Button')).toBeInTheDocument();
+
+  // Item removed from document
+  await waitForElementToBeRemoved(screen.queryByText('Button'));
+
+  // Item add to document
+  // await waitFor(() => {
+  //   return expect(screen.getByText('Button')).toBeInTheDocument();
+  // });
+});
---
+++ src/components/Async/index.tsx
@@ -0,0 +1,17 @@
+import { useEffect, useState } from 'react';
+
+export function Async() {
+  const [isButtonVisible, setIsButtonVisible] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsButtonVisible(true);
+    }, 1000);
+  }, []);
+  return (
+    <div>
+      <div>Hello world</div>
+      {!isButtonVisible && <button type="button">Button</button>}
+    </div>
+  );
+}
