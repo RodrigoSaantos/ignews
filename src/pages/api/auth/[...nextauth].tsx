6f0ccaae92ed0019cf3951b522e820151f470b08
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-17 07:54:30 -0300

    Saving user in the bank

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -1,5 +1,7 @@
 import NextAuth from 'next-auth';
 import GithubProvider from "next-auth/providers/github";
+import { query as q } from 'faunadb';
+import { fauna } from '../../../services/fauna';
 
 export default NextAuth({
   // Configure one or more authentication providers
@@ -10,4 +12,22 @@ export default NextAuth({
       authorization: "https://github.com/login/oauth/authorize?scope=read:user"
     }),
   ],
+
+  jwt: {
+    signingKey: process.env.SIGNING_KEY,
+  },
+
+  callbacks: {
+    async signIn({ user, account, profile }) {
+      const { email } = user;
+
+      try {
+        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
+
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  },
 });
