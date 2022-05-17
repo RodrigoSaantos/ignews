2065fca4823e3bf364a34ce18c4067d5f937a6da
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-17 08:11:20 -0300

    Checking for Duplicate User

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -15,11 +15,20 @@ export default NextAuth({
   callbacks: {
     async signIn({ user, account, profile }) {
       const { email } = user;
+      console.log(email);
 
       try {
-        await fauna.query(q.Create(q.Collection('users'), { data: { email } }));
-
-        return true;
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(q.Index('user_by_email'), q.Casefold(user.email)),
+              ),
+            ),
+            q.Create(q.Collection('users'), { data: { email } }),
+            q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
+          ),
+        );
       } catch {
         return false;
       }
