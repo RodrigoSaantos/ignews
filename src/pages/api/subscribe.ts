ff8c9f09f282a971bae7b84f575ad437c6a0f4cb
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-18 08:33:17 -0300

    Generating checkout session

---
+++ src/pages/api/subscribe.ts
@@ -0,0 +1,30 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getSession } from 'next-auth/react';
+import { stripe } from '../../services/stripe';
+
+export default async (request: NextApiRequest, response: NextApiResponse) => {
+  if (request.method === 'POST') {
+    const session = await getSession({ req: request });
+
+    const stripeCustomer = await stripe.customers.create({
+      email: session.user.email,
+    });
+
+    const stripeCheckoutSession = await stripe.checkout.sessions.create({
+      customer: stripeCustomer.id,
+      payment_method_types: ['card'],
+      billing_address_collection: 'required',
+      line_items: [{ price: 'price_1IcDBvLrPt1A6hda6KDNXoiY', quantity: 1 }],
+      mode: 'subscription',
+      allow_promotion_codes: true,
+      success_url: process.env.STRIPE_SUCCESS_URL,
+      cancel_url: process.env.STRIPE_CANCEL_URL,
+    });
+
+    return response.status(200).json({ sessionId: stripeCheckoutSession.id });
+  } else {
+    response.setHeader('Allow', 'POST');
+    response.status(405).end('Method not allowed');
+  }
+
+};
