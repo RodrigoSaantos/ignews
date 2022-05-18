f733bb27931404032c18ff09e349d1e100899251
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-05-18 09:00:11 -0300

    Redirecting to Stripe

--- src/pages/api/auth/[...nextauth].tsx
+++ src/pages/api/auth/[...nextauth].tsx
@@ -29,6 +29,7 @@ export default NextAuth({
             q.Get(q.Match(q.Index('user_by_email'), q.Casefold(user.email))),
           ),
         );
+        return true
       } catch {
         return false;
       }
