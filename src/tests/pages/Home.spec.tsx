d690ce3bb2e86fbe193464a5e6d444620c23dd20
Rodrigo Santos <contato.rodrigosaantos@gmail.com>
2022-06-28 22:05:07 -0300

    Testando getStaticProps

--- src/tests/pages/Home.spec.tsx
+++ src/tests/pages/Home.spec.tsx
@@ -21,4 +21,26 @@ describe('Home page', () => {
 
     expect(screen.getByText('for R$ 10,00 month')).toBeInTheDocument();
   });
+
+  it('loads initial data', async () => {
+    const retriveStripePricesMocked = jest.mocked(stripe.prices.retrieve);
+
+    retriveStripePricesMocked.mockResolvedValueOnce({
+      id: 'fake-price-id',
+      unit_amount: 1000,
+    } as any);
+
+    const response = await getStaticProps({});
+
+    expect(response).toEqual(
+      expect.objectContaining({
+        props: {
+          product: {
+            priceId: 'fake-price-id',
+            amount: '$10.00',
+          },
+        },
+      }),
+    );
+  });
 });
